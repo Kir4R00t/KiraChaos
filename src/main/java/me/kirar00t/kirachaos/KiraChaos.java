a86c3b94e3906896896b08238fe59a3ea1efdf83
Kir4R00t <miki4sa@interia.pl>
2024-04-09 18:03:36 +0200

    asd

---
+++ src/main/java/me/kirar00t/kirachaos/KiraChaos.java
@@ -0,0 +1,17 @@
+package me.kirar00t.kirachaos;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KiraChaos extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
